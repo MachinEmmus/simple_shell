94a966dcc4d86ac6cfbf79de5df45b997ffd8522
evertcolombia <evertescalante@gmail.com>
2019-11-24 22:05:35 +0000

    shell. h header file

---
+++ shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*allowed libraries*/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Enviroment variable*/
+extern char **environ;
+
+/*prototypes*/
+char *get_env(const char *path_name);
+void printCommand(**tokens);
+char **splitString(char *line);
+char **splitStringPath(char *arguments, char **argvs);
+char *get_line(void);
+
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
+
+#endif/*SHELL_H*/
