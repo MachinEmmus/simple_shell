e1b33b2594b2e0b4020d8a4cd2335cb69903b437
evertcolombia <evertescalante@gmail.com>
2019-11-27 21:51:22 +0000

    hotfix

--- tokenize.c
+++ tokenize.c
@@ -1,49 +1,37 @@
 #include "shell.h"
-
+/**
+ * splitString - tokenize the user input
+ * @arguments: arguments from the stdin
+ *
+ * Return: double pointer
+ */
 char **splitString(char *arguments)
 {
-	
 	char *delimitador = " \n:\t";
-
-	/*init a bufsize and a position = 0*/
-	int position = 0, arg_len = 1; /*bufer_size = BUFSIZE;*/
-
-	/*dobule pointer to create space in memory  fot the bufsize*/
+	int position = 0, arg_len = 1;
 	char **tokens;
-	/*will store the tokens*/
 	char *token;
 
 	arg_len = _strlen(arguments);
-	tokens = malloc (arg_len * sizeof(char*));
-	/*if malloc files exit*/
+	tokens = malloc(arg_len * sizeof(char *));
 	if (tokens == NULL)
-		return(NULL);
+		return (NULL);
 
-	/*take the line argument and splice it with the delimitors*/
 	token = strtok(arguments, delimitador);
-	/*loop while the token is not null*/
 	while (token != NULL)
 	{
-		/*token in position[position] will store what token in order brings*/
 		tokens[position] = token;
 		position++;
 
-		/*if position is equal or major than bufsize size*/
 		if (position >= arg_len)
 		{
-				/*bufsize will recives more buufersize  + the lsh_tok_bufsize*/
-				arg_len += arg_len;
-				/*reallocate the new tokens using realloc*/
-				tokens = realloc(tokens, arg_len * sizeof(char *));
-
-				/*if tokens ends*/
-				if (tokens == NULL)
-						return(NULL);
+			arg_len += arg_len;
+			tokens = realloc(tokens, arg_len * sizeof(char *));
+			if (tokens == NULL)
+				return (NULL);
 		}
-		/*token will be delimited with null*/
 		token = strtok(NULL, delimitador);
 	}
-	/*last potision of tokens is nulls*/
 	tokens[position] = NULL;
-	return tokens;
+	return (tokens);
 }
